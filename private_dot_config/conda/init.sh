9303993f9d2d595d36e7359a320b79650824a227
Holger Hoefling <hhoeflin@gmail.com>
2022-10-08 06:22:05 +0200

    update makefiles for errors, bashrc

--- private_dot_config/conda/init.sh
+++ private_dot_config/conda/init.sh
@@ -1,18 +1,19 @@
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/home/hhoeflin/mambaforge/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+MAMBA_PATH=$(brew --prefix mambaforge)
+__conda_setup="$(${MAMBA_PATH}'/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
-    if [ -f "/home/hhoeflin/mambaforge/etc/profile.d/conda.sh" ]; then
-        . "/home/hhoeflin/mambaforge/etc/profile.d/conda.sh"
+    if [ -f "${MAMBA_PATH}/etc/profile.d/conda.sh" ]; then
+        . "${MAMBA_PATH}/etc/profile.d/conda.sh"
     else
-        export PATH="/home/hhoeflin/mambaforge/bin:$PATH"
+        export PATH="${MAMBA_PATH}/bin:$PATH"
     fi
 fi
 unset __conda_setup
 
-if [ -f "/home/hhoeflin/mambaforge/etc/profile.d/mamba.sh" ]; then
-    . "/home/hhoeflin/mambaforge/etc/profile.d/mamba.sh"
+if [ -f "${MAMBA_PATH}/etc/profile.d/mamba.sh" ]; then
+    . "${MAMBA_PATH}/etc/profile.d/mamba.sh"
 fi
 # <<< conda initialize <<<
