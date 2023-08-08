fe6b9fa02f58182ff29208b15f15a88317af0d4d
Holger Hoefling <hhoeflin@gmail.com>
2023-08-08 22:46:30 +0200

    add diagnostic source to error messages

--- private_dot_config/exact_nvim/lua/config/nvim_lint.lua
+++ private_dot_config/exact_nvim/lua/config/nvim_lint.lua
@@ -10,3 +10,15 @@ autocmd BufWritePost * lua require('lint').try_lint()
 vim.cmd [[
 command LintAll :let buf=bufnr('%') | exec 'bufdo lua require("lint").try_lint()' | exec 'b' buf
 ]]
+
+vim.diagnostic.config({
+virtual_text = {
+    source = "always",
+    format = function(diagnostic)
+    if diagnostic.user_data.lsp.code ~= nil then
+        return string.format("%s: %s", diagnostic.user_data.lsp.code, diagnostic.message)
+    end
+    return diagnostic.message
+end,
+},
+})
