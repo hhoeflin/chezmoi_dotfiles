8a567279bf779572bc2cf98c0721f6653ed16e5c
Holger Hoefling <hhoeflin@gmail.com>
2023-08-09 21:11:33 +0200

    improve history search

--- private_dot_config/python/ipython_fzf_history.py
+++ private_dot_config/python/ipython_fzf_history.py
@@ -1,14 +1,21 @@
+import subprocess
 import sys
 
 from prompt_toolkit.enums import DEFAULT_BUFFER
 from prompt_toolkit.filters import HasFocus, HasSelection
 from prompt_toolkit.keys import Keys
 
-import pyfzf
-
 ipython = get_ipython()
-fzf = pyfzf.FzfPrompt()
 from shutil import which
+
+
+def fzf_prompt(input) -> str:
+    return subprocess.check_output(
+        "fzf --no-sort --multi --border --height=80% --margin=1 --padding=1"
+        " --preview 'echo {} | sed \"s/ @@ /\\n/g\" | bat --color=always --style=numbers -l py -'",
+        input=input,
+        text=True,
+    )
 
 
 def is_in_empty_line(buf):
@@ -24,29 +31,31 @@ def is_in_empty_line(buf):
 
 def fzf_i_search(event):
     history_set = set()
-    history_strings = [i[2] for i in ipython.history_manager.get_tail(5000)][::-1]
+    history_strings = [
+        i[2] for i in ipython.history_manager.get_tail(5000, include_latest=True)
+    ][::-1]
 
     # we replace newlines as fzf can only work on single lines; in the preview and later
     # we reverse this effect
-    history_strings = [
-        s.replace("\n", " @@ ")
-        for s in history_strings
-        if not (s in history_set or history_set.add(s))
-    ]
-
+    history_strings = [s.replace("\n", " @@ ") for s in dict.fromkeys(history_strings)]
     # refresh prompt
     print("", end="\r", flush=True)
     try:
-        text = fzf.prompt(
-            history_strings,
-            fzf_options="--no-sort --multi --border --height=80% --margin=1 --padding=1"
+        text = subprocess.check_output(
+            "fzf --no-sort --multi --border --height=80% --margin=1 --padding=1"
             " --preview 'echo {} | sed \"s/ @@ /\\n/g\" | bat --color=always --style=numbers -l py -'",
+            input="\n".join(history_strings),
+            text=True,
+            shell=True,
         )
         # multiple returns get concatenated with an emtpy line in-between
-        text = "\n\n".join(text)
+        text = text.rstrip().replace("\n", "\n\n")
         # reverse the replacement of newlines
         text = text.replace(" @@ ", "\n")
-    except:
+    except subprocess.CalledProcessError:
+        return
+    except Exception as e:
+        print(e)
         return
     buf = event.current_buffer
     if not is_in_empty_line(buf):
