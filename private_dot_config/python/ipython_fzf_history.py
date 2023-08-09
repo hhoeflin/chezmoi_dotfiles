09178e9d54478811e8205b2a17be9936cb14da84
Holger Hoefling <hhoeflin@gmail.com>
2023-08-09 13:11:28 +0200

    fzf in python for command history search

---
+++ private_dot_config/python/ipython_fzf_history.py
@@ -0,0 +1,66 @@
+import sys
+
+from prompt_toolkit.enums import DEFAULT_BUFFER
+from prompt_toolkit.filters import HasFocus, HasSelection
+from prompt_toolkit.keys import Keys
+
+import pyfzf
+
+ipython = get_ipython()
+fzf = pyfzf.FzfPrompt()
+from shutil import which
+
+
+def is_in_empty_line(buf):
+    text = buf.text
+    cursor_position = buf.cursor_position
+    text = text.split("\n")
+    for line in text:
+        if len(line) >= cursor_position:
+            return not line
+        else:
+            cursor_position -= len(line) + 1
+
+
+def fzf_i_search(event):
+    history_set = set()
+    history_strings = [i[2] for i in ipython.history_manager.get_tail(5000)][::-1]
+
+    # we replace newlines as fzf can only work on single lines; in the preview and later
+    # we reverse this effect
+    history_strings = [
+        s.replace("\n", " @@ ")
+        for s in history_strings
+        if not (s in history_set or history_set.add(s))
+    ]
+
+    # refresh prompt
+    print("", end="\r", flush=True)
+    try:
+        text = fzf.prompt(
+            history_strings,
+            fzf_options="--no-sort --border --height=80% --margin=1 --padding=1"
+            " --preview 'echo {} | sed \"s/ @@ /\\n/g\" | bat --color=always --style=numbers -l py -'",
+        )
+        # reverse the replacement of newlines
+        text = text.replace(" @@ ", "\n")
+    except:
+        return
+    buf = event.current_buffer
+    if not is_in_empty_line(buf):
+        buf.insert_line_below()
+    buf.insert_text("\n".join(text))
+
+
+def register():
+    # Register the shortcut if IPython is using prompt_toolkit
+    # for this we need fzf and bat
+    if not which("fzf"):
+        raise SystemError("fzf could not be found on path")
+    if not which("bat"):
+        raise SystemError("bat could not be found on path")
+    if getattr(ipython, "pt_app", None):
+        registry = ipython.pt_app.key_bindings
+        registry.add_binding(
+            Keys.ControlR, filter=(HasFocus(DEFAULT_BUFFER) & ~HasSelection())
+        )(fzf_i_search)
--- private_dot_config/python/pythonrc.py
+++ private_dot_config/python/pythonrc.py
@@ -1,9 +1,39 @@
+import pathlib
 import sys
 
+
+class add_sys_path:
+    def __init__(self, path):
+        self.path = path
+
+    def __enter__(self):
+        sys.path.insert(0, self.path)
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        try:
+            sys.path.remove(self.path)
+        except ValueError:
+            pass
+
+
+print(__file__)
 # check if IPython is loaded
 if "IPython" in sys.modules:
     # yes it is
     try:
         get_ipython().run_line_magic("load_ext", "mkreports")
-    except Exception:
-        pass
+    except Exception as e:
+        print(e)
+
+    with add_sys_path(str(pathlib.Path(__file__).parent)):
+        try:
+            import ipython_fzf_history
+
+            ipython_fzf_history.register()
+            del ipython_fzf_history
+        except Exception as e:
+            print(e)
+
+del add_sys_path
+del sys
+del pathlib
