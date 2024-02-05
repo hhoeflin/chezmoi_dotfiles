ebf6ce70b438d6ccb349c0fcd37d4770f7d7e351
Holger Hoefling <hhoeflin@gmail.com>
2024-02-05 23:13:33 +0100

    remove unnecessary files

--- private_dot_config/exact_nvim/lua/config/slime.lua
+++
@@ -1,6 +0,0 @@
-vim.g.slime_bracketed_paste = 1
-vim.g.slime_target = "tmux"
-
--- remap so that it jumps to the end of the executed code
---vim.api.nvim_set_keymap('x', '<c-c><c-c>', '<Plug>SlimeRegionSend gv<esc>', {silent=true, noremap=true})
---vim.api.nvim_set_keymap('n', '<c-c><cr>', '<c-c><c-c>}j', {silent=true, noremap=false})
