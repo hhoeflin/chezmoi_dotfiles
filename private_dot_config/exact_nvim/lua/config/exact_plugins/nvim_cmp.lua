ba8f68a423ca71932b6f6ff698ef002f321aa05a
Holger Hoefling <hhoeflin@gmail.com>
2024-02-06 23:23:28 +0100

    incorporate github copilot

--- private_dot_config/exact_nvim/lua/config/exact_plugins/nvim_cmp.lua
+++ private_dot_config/exact_nvim/lua/config/exact_plugins/nvim_cmp.lua
@@ -1,27 +1,46 @@
 local M = {
 	"hrsh7th/nvim-cmp",
 	dependencies = {
-        "neovim/nvim-lspconfig",
+		"neovim/nvim-lspconfig",
 		"hrsh7th/cmp-nvim-lsp",
 		"hrsh7th/cmp-nvim-lua",
 		"hrsh7th/cmp-buffer",
 		"hrsh7th/cmp-path",
 		"hrsh7th/cmp-cmdline",
-        "hrsh7th/cmp-nvim-lsp-signature-help",
-
+		"hrsh7th/cmp-nvim-lsp-signature-help",
+		"zbirenbaum/copilot-cmp",
+		"onsails/lspkind.nvim",
 	},
 }
 
 M.config = function()
 	local cmp = require("cmp")
 	vim.opt.completeopt = { "menu", "menuone", "noselect" }
-
-    local has_words_before = function()
-      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-    end
-	
-    cmp.setup({
+	local has_words_before = function()
+		if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+			return false
+		end
+		local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+		return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+	end
+	local lspkind = require("lspkind")
+	cmp.setup({
+		formatting = {
+			format = lspkind.cmp_format({
+				mode = "symbol", -- show only symbol annotations
+				maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+				-- can also be a function to dynamically calculate max width such as
+				-- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+				ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+				show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+				symbol_map = { Copilot = "" },
+				-- The function below will be called before any actual modifications from lspkind
+				-- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+				before = function(entry, vim_item)
+					return vim_item
+				end,
+			}),
+		},
 		snippet = {
 			expand = function(args)
 				require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
@@ -32,31 +51,30 @@ M.config = function()
 			-- documentation = cmp.config.window.bordered(),
 		},
 		mapping = cmp.mapping.preset.insert({
-             ["<Tab>"] = cmp.mapping(function(fallback)
-               if cmp.visible() then
-                 cmp.select_next_item()
-               elseif has_words_before() then
-                 cmp.complete()
-               else
-                 fallback()
-               end
-            end, { "i", "s" }),
-            ["<S-Tab>"] = cmp.mapping(function(fallback)
-              if cmp.visible() then
-                cmp.select_prev_item()
-              else
-                fallback()
-              end
-            end, { "i", "s" }),
+			["<Tab>"] = cmp.mapping(function(fallback)
+				if cmp.visible() and has_words_before() then
+					cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+				else
+					fallback()
+				end
+			end, { "i", "s" }),
+			["<S-Tab>"] = cmp.mapping(function(fallback)
+				if cmp.visible() then
+					cmp.select_prev_item()
+				else
+					fallback()
+				end
+			end, { "i", "s" }),
 			["<C-b>"] = cmp.mapping.scroll_docs(-4),
 			["<C-f>"] = cmp.mapping.scroll_docs(4),
 			["<C-Space>"] = cmp.mapping.complete(),
 			["<C-e>"] = cmp.mapping.abort(),
-			["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+			["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 		}),
 		sources = cmp.config.sources({
+			{ name = "copilot" },
 			{ name = "nvim_lsp" },
-            { name = "nvim_lsp_signature_help" },
+			{ name = "nvim_lsp_signature_help" },
 			{ name = "nvim_lua" },
 			{ name = "luasnip" }, -- For luasnip users.
 			-- { name = "orgmode" },
@@ -64,6 +82,24 @@ M.config = function()
 			{ name = "buffer" },
 			{ name = "path" },
 		}),
+		sorting = { -- introduced for copilot; otherwise not necessary
+			priority_weight = 2,
+			comparators = {
+				require("copilot_cmp.comparators").prioritize,
+
+				-- Below is the default comparitor list and order for nvim-cmp
+				cmp.config.compare.offset,
+				-- cmp.config.compare.scopes, --this is commented in nvim-cmp too
+				cmp.config.compare.exact,
+				cmp.config.compare.score,
+				cmp.config.compare.recently_used,
+				cmp.config.compare.locality,
+				cmp.config.compare.kind,
+				cmp.config.compare.sort_text,
+				cmp.config.compare.length,
+				cmp.config.compare.order,
+			},
+		},
 	})
 
 	cmp.setup.cmdline(":", {
