f58bf29b673f4d6366bc9bb0b436b062f7059d00
Holger Hoefling <hhoeflin@gmail.com>
2024-02-10 16:08:11 +0100

    add outline plugin and vim-slime lualine indicator

--- private_dot_config/exact_nvim/lua/config/exact_plugins/lualine.lua
+++ private_dot_config/exact_nvim/lua/config/exact_plugins/lualine.lua
@@ -1,18 +1,32 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    lazy=false,
-    dependencies={'nvim-tree/nvim-web-devicons'},
-    opts={
-        sections = {
-          lualine_c = {
-            {
-              'filename',
-              file_status = true,  -- displays file status (readonly status, modified status)
-              path = 1,            -- 0 = just filename, 1 = relative path, 2 = absolute path
-              shorting_target = 40 -- Shortens path to leave 40 space in the window
-                                   -- for other components. Terrible name any suggestions?
-            }
-          }
-        }
-    }
+	"nvim-lualine/lualine.nvim",
+	lazy = false,
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {
+		sections = {
+			lualine_c = {
+				{
+					"filename",
+					file_status = true, -- displays file status (readonly status, modified status)
+					path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
+					shorting_target = 40, -- Shortens path to leave 40 space in the window
+					-- for other components. Terrible name any suggestions?
+				},
+			},
+			lualine_x = {
+				{
+					function()
+						if vim.fn.exists("b:slime_config") ~= 0 then
+							return "💩 " .. vim.b.slime_config["target_pane"]
+						else
+							return "💩 -"
+						end
+					end,
+				},
+				"encoding",
+				"fileformat",
+				"filetype",
+			},
+		},
+	},
 }
--- private_dot_config/exact_nvim/lua/config/exact_plugins/vim_slime.lua
+++ private_dot_config/exact_nvim/lua/config/exact_plugins/vim_slime.lua
@@ -1,13 +1,13 @@
 return {
-    'jpalardy/vim-slime',
-    lazy=true,
-    keys={
-        {'<C-c><C-c>'},
-        {'<C-c>v'}
-    },
-    config=function()
-        vim.g.slime_bracketed_paste = 1
-        vim.g.slime_target = "tmux"
-    end,
-
+	"jpalardy/vim-slime",
+	lazy = true,
+	cmd = "SlimeConfig",
+	config = function()
+		vim.g.slime_bracketed_paste = 1
+		vim.g.slime_target = "tmux"
+		-- don't jump back to the original cursor position
+		-- vim.g.slime_preserve_curpos = 0
+		-- require("vim-slime").setup()
+		vim.keymap.set("x", "<C-c><C-c>", ":<c-u>call slime#send_op(visualmode(), 1)<cr>`>G")
+	end,
 }
