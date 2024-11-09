6a031a0eb4561eedcfae76bb697b2473ef7a2fb1
Holger Hoefling <hhoeflin@gmail.com>
2024-11-09 15:47:00 +0100

    move all things nvim_dap into one file

--- private_dot_config/exact_nvim/lua/config/exact_plugins/nvim_dap.lua
+++ private_dot_config/exact_nvim/lua/config/exact_plugins/nvim_dap.lua
@@ -106,4 +106,19 @@ return {
 			end
 		end,
 	},
+	{
+		"mfussenegger/nvim-dap-python",
+		dependencies = {
+			"mfussenegger/nvim-dap",
+      -- stylua: ignore
+      keys = {
+        { "<leader>dPt", function() require('dap-python').test_method() end, desc = "Debug Method" },
+        { "<leader>dPc", function() require('dap-python').test_class() end, desc = "Debug Class" },
+      },
+			config = function()
+				local python_path = "~/.local/progs/pipx/latest/venvs/debugpy/bin/python"
+				require("dap-python").setup(python_path)
+			end,
+		},
+	},
 }
