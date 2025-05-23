4f59f85131e3e2802f1f73f8704d52398f129f66
Holger Hoefling <hhoeflin@gmail.com>
2025-05-23 05:14:47 +0200

    fix loading lsp after lazy config

--- private_dot_config/exact_nvim/init.lua
+++ private_dot_config/exact_nvim/init.lua
@@ -14,10 +14,6 @@ vim.opt.rtp:prepend(lazypath)
 
 -- Load the configuration
 require("settings")
-require("lsp_settings")
 
-if vim.loop.fs_stat(".nvim.lua") then
-	vim.opt.exrc = true -- allow local .nvim.lua .vimrc .exrc files
-else
-	require("config")()
-end
+require("config")()
+require("lsp_settings")
